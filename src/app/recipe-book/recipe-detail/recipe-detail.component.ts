a5c71439c0da25adf26bebb07b05d1547481980a
ErinKace <erinkacerovskis@gmail.com>
2024-05-27 12:16:43 -0500

    Well its sorta working

--- src/app/recipe-book/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe-book/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { ShoppingListService } from 'src/app/shared/shopping-list.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,11 +10,23 @@ import { ShoppingListService } from 'src/app/shared/shopping-list.service';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe!: Recipe;
+  recipe!: Recipe;
+  recipeId!: number;
 
-  constructor(private shoppingService: ShoppingListService) { }
+  constructor(private shoppingService: ShoppingListService, private recipeService: RecipeService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    let recipeId = this.route.params.subscribe((params: Params) => {
+      this.recipeId = +params['id'];
+      this.recipeService.selectRecipeById(this.recipeId);
+      this.recipeService.recipeSelected.subscribe(
+        (recipe:Recipe)=> {
+          this.recipe = recipe;
+        }
+      )
+    })
+
+
   }
 
   onAddToShoppinglist() {
