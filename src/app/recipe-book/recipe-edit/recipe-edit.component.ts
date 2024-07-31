1cd284d94d0154604a650fa2f9078ee08d692ffe
ErinKace <erinkacerovskis@gmail.com>
2024-07-31 17:08:41 -0500

    working on recipe edit form

--- src/app/recipe-book/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe-book/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
@@ -9,6 +10,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id!: number;
   editMode = false;
+  recipeForm!: FormGroup;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -20,6 +22,13 @@ export class RecipeEditComponent implements OnInit {
         this.editMode = params['id'] != null;
         console.log("Edit Mode:"+this.editMode);
     })
+
+  }
+
+  private initForm() {
+    this.recipeForm = new FormGroup({
+
+    });
 
   }
 
